7236bf6720ba78964c593f0d5e890d296b43d917
kyrillosezzat <kyrillosezzat300@gmail.com>
2019-08-27 15:08:20 -0700

    Updates

---
+++ precache-manifest.b00024561204a4b185292f6231bb1d7e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df69f677d3df9b2760817cd9c400fe0e",
+    "url": "/React-Redux-MobileShop/index.html"
+  },
+  {
+    "revision": "e12091e697b6820948d1",
+    "url": "/React-Redux-MobileShop/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4dd934dae4041459a7c0",
+    "url": "/React-Redux-MobileShop/static/css/main.e9082f16.chunk.css"
+  },
+  {
+    "revision": "e12091e697b6820948d1",
+    "url": "/React-Redux-MobileShop/static/js/2.03311760.chunk.js"
+  },
+  {
+    "revision": "4dd934dae4041459a7c0",
+    "url": "/React-Redux-MobileShop/static/js/main.5ad4bcef.chunk.js"
+  },
+  {
+    "revision": "61680f32065d7b8a33a6",
+    "url": "/React-Redux-MobileShop/static/js/runtime~main.40eb462f.js"
+  },
+  {
+    "revision": "d63d9eff35aa057bab9c5041938d6d69",
+    "url": "/React-Redux-MobileShop/static/media/product-1.d63d9eff.png"
+  },
+  {
+    "revision": "ad7ef891046b2a56aa41db25cbcf2a9b",
+    "url": "/React-Redux-MobileShop/static/media/product-2.ad7ef891.png"
+  },
+  {
+    "revision": "d27ec2bc66073aa4b0b80368a9ac410e",
+    "url": "/React-Redux-MobileShop/static/media/product-3.d27ec2bc.png"
+  },
+  {
+    "revision": "d9ed75372e686c2f8c38a53a52482e66",
+    "url": "/React-Redux-MobileShop/static/media/product-4.d9ed7537.png"
+  },
+  {
+    "revision": "8f9a1f89508713ff90dc1b497c92b1b2",
+    "url": "/React-Redux-MobileShop/static/media/product-5.8f9a1f89.png"
+  },
+  {
+    "revision": "b49d1b7cf1b82dad0a8059302706685a",
+    "url": "/React-Redux-MobileShop/static/media/product-6.b49d1b7c.png"
+  },
+  {
+    "revision": "4c6ab223c32bec22d9cd6070caaedf19",
+    "url": "/React-Redux-MobileShop/static/media/product-7.4c6ab223.png"
+  },
+  {
+    "revision": "d390282f89018d8777d36210b7df6d7b",
+    "url": "/React-Redux-MobileShop/static/media/product-8.d390282f.png"
+  }
+]);
